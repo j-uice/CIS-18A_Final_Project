8d4d2597c19c475aa1189f865d1ca9e899ce441a
j-uice <jtwroldan08@gmail.com>
2025-02-13 00:45:32 -0800

    Create Crt.java

---
+++ Program/F/Crt.java
@@ -0,0 +1,13 @@
+package F;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Crt {
+	 public static int[] quantities = {0, 0, 0, 0, 0};	// Stores quantities for all products
+   	 public static JLabel[] productLabels; 			// Stores labels for dynamic updates
+
+	 public static String[] product = { "FLASHLIGHT", "THERMAL WATER BOTTLE", "THERMAL FOOD CONTAINER", 
+			 "NON-PERISHABLE PACKAGE", "MED-KIT"};
+	 public static double[] prices = {6.79, 11.45, 20.00, 28.99, 24.99};
+}
